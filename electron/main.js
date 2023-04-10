72b5bf350678b184eef8b664d5f06c9addd3772d
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-04-10 20:40:19 +0200

    Initial commit

---
+++ electron/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
